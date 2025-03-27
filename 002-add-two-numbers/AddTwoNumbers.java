b8f733a1cfa30fb106309995af89bc7a3f097b59
huntersteffner <huntersteffner@gmail.com>
2025-03-27 12:16:54 -0400

    002 Add Two Numbers

--- 002-add-two-numbers/AddTwoNumbers.java
+++ 002-add-two-numbers/AddTwoNumbers.java
@@ -1,4 +1,50 @@
+
+
 public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        ListNode exampleOneListOne = new ListNode(
+            2, new ListNode(
+                4, new ListNode(3)
+            )
+        );
+        ListNode exampleOneListTwo = new ListNode(
+            5, new ListNode(
+                6, new ListNode(4)
+            )
+        );
+
+        ListNode exampleTwoListOne = new ListNode(0);
+        ListNode exampleTwoListTwo = new ListNode(0);
+
+        ListNode exampleThreeListOne = new ListNode(9, new ListNode(
+            9, new ListNode(
+                9, new ListNode(
+                    9, new ListNode(
+                        9, new ListNode(
+                            9, new ListNode(
+                                9
+                            )
+                        )
+                    )
+                )
+            )
+        ));
+        ListNode exampleThreeListTwo = new ListNode(9, new ListNode(
+            9, new ListNode(
+                9, new ListNode(
+                    9
+                )
+            )
+        ));
+
+
+        ListNode exampleOne = addTwoNumbers(exampleOneListOne, exampleOneListTwo);
+        ListNode exampleTwo = addTwoNumbers(exampleTwoListOne, exampleTwoListTwo);
+        ListNode exampleThree = addTwoNumbers(exampleThreeListOne, exampleThreeListTwo);
+
+    }
 
     /**
  * Definition for singly-linked list.
@@ -10,15 +56,55 @@ public class AddTwoNumbers {
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode();
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode(0);
+        ListNode other = l3;
+        int carryOver = 0;
+
+        int total = 0;
+        
+        while (l1 != null || l2 != null) {
+
+            total = 0;
+
+            if (l1 != null) {
+                total += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                total += l2.val;
+                l2 = l2.next;
+            }
+
+            total += carryOver;
+            if (total >= 10 ) {
+                total -= 10;
+                carryOver = 1;
+            } else {
+                carryOver = 0;
+            }
+            // l3.val = total;
+            other.next = new ListNode(total);
+            other = other.next;
+        }
+        if (carryOver > 0) {
+            other.next = new ListNode(carryOver);
+        }
+        return l3;
     }
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+
 }
