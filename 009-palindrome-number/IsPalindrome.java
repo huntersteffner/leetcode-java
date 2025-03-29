0c874718501dbd39432e13e04df1b0283da55981
huntersteffner <huntersteffner@gmail.com>
2025-03-29 19:58:10 -0400

    009-palindrom-number

--- 009-palindrome-number/IsPalindrome.java
+++ 009-palindrome-number/IsPalindrome.java
@@ -1,5 +1,20 @@
 public class IsPalindrome {
-    public boolean isPalindrome(int x) {
-        return true;
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+    public static boolean isPalindrome(int x) {
+
+        String xString = Integer.toString(x);
+
+        String reversed = "";
+        
+        for (int i = xString.length() - 1; i >= 0; i--) {
+            reversed += Character.toString(xString.charAt(i));
+        }
+
+        return xString.equals(reversed);
     }
 }
