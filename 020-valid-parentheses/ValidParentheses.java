1ef245548ecf3386219f0ae5366bcc83c68d86ca
huntersteffner <huntersteffner@gmail.com>
2025-04-02 15:41:37 -0400

    020 Valid Parentheses

--- 020-valid-parentheses/ValidParentheses.java
+++ 020-valid-parentheses/ValidParentheses.java
@@ -1,7 +1,37 @@
+import java.util.Stack;
+
 public class ValidParentheses {
-    class Solution {
-        public boolean isValid(String s) {
-            return true;
+    public static void main(String[] args) {
+
+        
+        System.out.println(isValid("({[]()}[(){()}])"));
+        System.out.println(isValid("({[]()}[(){())])"));
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println(isValid("([])"));
+    }
+    
+    public static boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int index = 0; index < s.length(); index++) {
+            if (s.charAt(index) == '(') {
+                stack.push(')');
+            } else if (s.charAt(index) == '[') {
+                stack.push(']');
+            } else if (s.charAt(index) == '{') {
+                stack.push('}');
+            } else {
+                if (s.charAt(index) == stack.peek()) {
+                    stack.pop();
+                } else {
+                    break;
+                }
+            }
         }
+
+        return stack.empty();
     }
 }
