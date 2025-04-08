c7a99fcb0283b09bef3abc5399214df7d6e756a6
huntersteffner <huntersteffner@gmail.com>
2025-04-08 14:52:18 -0400

    026 Remove Duplicate

--- 026-remove-duplicates-from-sorted-array/RemoveDuplicates.java
+++ 026-remove-duplicates-from-sorted-array/RemoveDuplicates.java
@@ -1,5 +1,22 @@
 public class RemoveDuplicates {
-    public int removeDuplicates(int[] nums) {
-        return 3;
+    public static void main(String[] args) {
+        int[] exampleOne = {
+            1,1,2
+        };
+        System.out.println(removeDuplicates(exampleOne));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[k]) {
+                k++;
+                nums[k] = nums[i];
+            }
+        }
+
+        return k + 1;
     }    
 }
