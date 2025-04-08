d7e434179542f5a04fd6cb973a55ec11ced97e31
huntersteffner <huntersteffner@gmail.com>
2025-04-07 20:25:56 -0400

    021 - Merge Sort

--- 021-merge-two-sorted-lists/MergeTwoLists.java
+++ 021-merge-two-sorted-lists/MergeTwoLists.java
@@ -9,11 +9,76 @@ public class MergeTwoLists {
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return new ListNode();
+    public static void main(String[] args) {
+        ListNode exampleOneListOne = new ListNode(
+            1, new ListNode(
+                2, new ListNode(
+                    4
+                )
+            )
+        );
+
+        ListNode exampleOneListTwo = new ListNode(
+            1, new ListNode(
+                3, new ListNode(
+                    4
+                )
+            )
+        );
+
+        ListNode exampleTwoListOne = new ListNode(
+            -9, new ListNode(
+                3
+            )
+        );
+
+        ListNode exampleTwoListTwo = new ListNode(
+            5, new ListNode(
+                7
+            )
+        );
+
+
+        // System.out.println(mergeTwoLists(exampleOneListOne, exampleOneListTwo));
+        System.out.println(mergeTwoLists(exampleTwoListOne, exampleTwoListTwo));
     }
 
-    public class ListNode {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode head = new ListNode();
+        ListNode current = head;
+
+        while (list1 != null && list2 != null) {
+            
+            if (list1.val < list2.val) {
+                current.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                current.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        while (list1 != null) {
+            current.next = new ListNode(list1.val);
+            list1 = list1.next;
+            current = current.next;
+        }
+
+        while (list2 != null) {
+            current.next = new ListNode(list2.val);
+            list2 = list2.next;
+            current = current.next;
+        }
+
+
+
+        return head.next;
+    }
+
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
