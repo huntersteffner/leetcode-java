6bc9648f69bd9c3b2ca5fc2cfd3d8816e61c27c4
huntersteffner <huntersteffner@gmail.com>
2025-04-09 11:00:19 -0400

    028 Search String

--- 028-search-string/StringSearch.java
+++ 028-search-string/StringSearch.java
@@ -1,5 +1,12 @@
 public class StringSearch {
-    public int strStr(String haystack, String needle) {
-        return 3;
+
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
+        System.out.println(strStr("leetcode", "leeto"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        
+        return haystack.indexOf(needle);
     }
 }
