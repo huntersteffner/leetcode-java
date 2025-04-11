620cc5e4beab19dede9024a103c904766ad2ea7a
huntersteffner <huntersteffner@gmail.com>
2025-04-11 15:08:51 -0400

    35 Search Insert

--- 035-search-insert/SearchInsert.java
+++ 035-search-insert/SearchInsert.java
@@ -1,5 +1,31 @@
 public class SearchInsert {
-    public int searchInsert(int[] nums, int target) {
-        return 3;
+    public static void main(String[] args) {
+        int[] exampleOne = {1,3,5,6};
+
+        System.out.println(searchInsert(exampleOne, 0));
+        System.out.println(searchInsert(exampleOne, 5));
+        System.out.println(searchInsert(exampleOne, 2));
+        System.out.println(searchInsert(exampleOne, 7));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+        int midPoint = 0;
+
+        while(low <= high) {
+            midPoint = (low + high) / 2;
+            int midNumber = nums[midPoint];
+
+            if (midNumber == target) {
+                return midPoint;
+            } else if (midNumber < target) {
+                low = midPoint + 1;
+            } else {
+                high = midPoint - 1;
+            }
+        }
+        return low;
     }
 }
