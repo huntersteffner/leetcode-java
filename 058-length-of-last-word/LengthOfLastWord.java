c2c2c11540b000bc890057b2346750afb2b2abcf
huntersteffner <huntersteffner@gmail.com>
2025-04-14 14:14:05 -0400

    058 Length of Last Word

--- 058-length-of-last-word/LengthOfLastWord.java
+++ 058-length-of-last-word/LengthOfLastWord.java
@@ -1,5 +1,14 @@
 public class LengthOfLastWord {
-    public int lengthOfLastWord(String s) {
-        return 3;
+    public static void main(String[] args) {
+
+        System.out.println(lengthOfLastWord("Hello World"));
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+        System.out.println(lengthOfLastWord("luffy is still joyboy"));
+    }
+
+
+    public static int lengthOfLastWord(String s) {
+        String[] arrString = s.split(" ");
+        return arrString[arrString.length - 1].length();
     }
 }
