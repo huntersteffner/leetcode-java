b3cef54a635f3c5c85fda50429189f72d9d2ad50
huntersteffner <huntersteffner@gmail.com>
2025-04-21 14:42:23 -0400

    069 Square Root

--- 069-Square-Root/SquareRoot.java
+++ 069-Square-Root/SquareRoot.java
@@ -1,5 +1,12 @@
 public class SquareRoot {
-    public int mySqrt(int x) {
-        return 3;
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(4));
+        System.out.println(mySqrt(8));
+    }
+
+    public static int mySqrt(int x) {
+        int sqrt = (int) Math.sqrt(x);
+        return sqrt;
     }
 }
