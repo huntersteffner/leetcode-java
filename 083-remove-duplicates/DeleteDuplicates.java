c0c758bccb64af5f734aa89e3e8d81a684c29558
huntersteffner <huntersteffner@gmail.com>
2025-04-24 14:55:28 -0400

    083 Remove Duplicates

--- 083-remove-duplicates/DeleteDuplicates.java
+++ 083-remove-duplicates/DeleteDuplicates.java
@@ -10,11 +10,75 @@
  */
 
 public class DeleteDuplicates {
-    public ListNode deleteDuplicates(ListNode head) {
-        return new ListNode();
+
+    public static void main(String[] args) {
+        ListNode exampleOne = new ListNode(1, new ListNode(
+            1, new ListNode(
+                2
+            )
+        ));
+
+        ListNode exampleTwo = new ListNode(1, new ListNode(
+            1, new ListNode(
+                2, new ListNode(
+                    3, new ListNode(
+                        3
+                    )
+                )
+            )
+        ));
+
+        ListNode exampleThree = new ListNode(-3, new ListNode(
+            1, new ListNode(
+                1, new ListNode(
+                    0, new ListNode(
+                        2, new ListNode(
+                            2, new ListNode(
+                                3, new ListNode(
+                                    3
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        ));
+
+        ListNode exampleFour = new ListNode(0, new ListNode(
+            0, new ListNode(
+                0, new ListNode(
+                    0
+                )
+            )
+        ));
+
+        ListNode answerFour = deleteDuplicates(exampleFour);
+        ListNode answerOne = deleteDuplicates(exampleOne);
+        ListNode answerTwo = deleteDuplicates(exampleTwo);
+        ListNode answerThree = deleteDuplicates(exampleThree);
+
+    }
+    public static ListNode deleteDuplicates(ListNode head) {
+
+
+        ListNode dummyHead = new ListNode(-101);
+        ListNode traverse = dummyHead;
+
+        while(head != null) {
+            
+            if (traverse.val != head.val) {
+                traverse.next = new ListNode(head.val);
+                traverse = traverse.next;
+            }
+            head = head.next;
+        }
+
+        
+
+        return dummyHead.next;
     }
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
