1bf4629cc12a25b0bd9b5dd15588cb160a2ecc25
huntersteffner <huntersteffner@gmail.com>
2025-05-20 14:39:42 -0400

    094 - In Order Traversal

--- 094-binary-tree-inorder-traversal/BinaryTreeInorderTraversal.java
+++ 094-binary-tree-inorder-traversal/BinaryTreeInorderTraversal.java
@@ -1,12 +1,78 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreeInorderTraversal {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = null;
+
+    public static void main(String[] args) {
+        TreeNode exampleOne = new TreeNode(
+            1,
+            null,
+            new TreeNode(
+                2,
+                new TreeNode(3),
+                null
+            )
+        );
+        TreeNode exampleTwo = new TreeNode(
+            1,
+            new TreeNode(
+                2,
+                new TreeNode(
+                    4
+                ),
+                new TreeNode(
+                    5,
+                    new TreeNode(
+                        6
+                    ),
+                    new TreeNode(
+                        7
+                    )
+                )
+            ),
+            new TreeNode(
+                3,
+                null,
+                new TreeNode(
+                    8,
+                    new TreeNode(
+                        9
+                    ),
+                    null
+                )
+            )
+        );
+
+        System.out.println(inorderTraversal(exampleOne));
+        System.out.println(inorderTraversal(exampleTwo));
+
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        TreeNode current = root;
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> s = new Stack<>();
+        // s.add(current);
+
+        while (current != null || !s.empty()) {
+            // current = current.left;
+            
+            while (current != null) {
+                s.add(current);
+                current = current.left;
+            }
+
+            // current = s.pop();
+            TreeNode top = s.pop();
+            current = top.right;
+            list.add(top.val);
+            
+        }
         return list;
     }
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
