2c73c446393d7fb65cf4d54ecd1c3a8ef3ebe7d3
huntersteffner <huntersteffner@gmail.com>
2025-05-21 11:51:28 -0400

    104 - Max Depth

--- 104-max-depth-of-binary-tree/MaxDepth.java
+++ 104-max-depth-of-binary-tree/MaxDepth.java
@@ -1,9 +1,55 @@
 public class MaxDepth {
-    public int maxDepth(TreeNode root) {
-        return 3;
+
+    public static void main(String[] args) {
+        TreeNode exampleOne = new TreeNode(
+            3,
+            new TreeNode(
+                9
+            ),
+            new TreeNode(
+                20,
+                new TreeNode(
+                    15
+                ),
+                new TreeNode(
+                    7
+                )
+            )
+        );
+
+        TreeNode exampleTwo = new TreeNode(
+            1,
+            null,
+            new TreeNode(
+                2
+            )
+        );
+
+        TreeNode exampleThree = new TreeNode(
+            1,
+            new TreeNode(
+                2
+                ),
+            null
+        );
+
+
+        System.out.println(maxDepth(exampleThree));
+        System.out.println(maxDepth(exampleOne));
+        System.out.println(maxDepth(exampleTwo));
     }
 
-    public class TreeNode {
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int max = Math.max(maxDepth(root.left), maxDepth(root.right));
+
+        return max + 1;
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
