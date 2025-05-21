e194bea8bd62ced0affecf64be09f98a11a607e6
huntersteffner <huntersteffner@gmail.com>
2025-05-21 15:49:44 -0400

    108 Covert array to bst

--- 108-convert-array-to-bst/ArrayToBST.java
+++ 108-convert-array-to-bst/ArrayToBST.java
@@ -1,10 +1,42 @@
+import java.util.Arrays;
+
 public class ArrayToBST {
 
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return new TreeNode();
+    public static void main(String[] args) {
+        int[] exampleOne = {-10,-3,0,5,9};
+        int[] exampleTwo = {1,3};
+
+        TreeNode answerOne = sortedArrayToBST(exampleOne);
+        TreeNode answerTwo = sortedArrayToBST(exampleTwo);
     }
 
-    public class TreeNode {
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 1) {
+            return new TreeNode(nums[0]);
+        }
+
+        if (nums.length == 2) {
+            return new TreeNode(
+                nums[0],
+                null,
+                new TreeNode(nums[1])
+            );
+        }
+
+        int mid = nums.length / 2;
+        int[] left = Arrays.copyOfRange(nums, 0, mid);
+        int[] right = Arrays.copyOfRange(nums, mid + 1, nums.length);
+
+        TreeNode node = new TreeNode(
+            nums[mid],
+            sortedArrayToBST(left),
+            sortedArrayToBST(right)
+        );
+
+        return node;
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
